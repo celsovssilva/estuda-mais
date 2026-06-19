7bb287bc733ed8b990ca4fe9e5f4415cc2213088
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-06-19 08:25:39 -0300

    configurando segurança e resolvendo bugs do código

--- backend/backend/src/main/java/com/estudamais/backend/repository/UserRepository.java
+++ backend/backend/src/main/java/com/estudamais/backend/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.estudamais.backend.repository;
 import com.estudamais.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
-    boolean existsByEmail(String email);
+   Optional <User> existsByEmail(String email);
 }
---
+++ backend/backend/src/main/java/com/estudamais/backend/service/serviceimpl/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.estudamais.backend.service.serviceimpl;
+
+import com.estudamais.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findby(email).orElseThrow(()-> new UsernameNotFoundException("user não encontrado" + email))
+    }
+}

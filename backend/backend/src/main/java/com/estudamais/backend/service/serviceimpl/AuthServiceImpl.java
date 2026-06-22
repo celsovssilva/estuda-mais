04f4c1f75ee32c3cb38fac2b2d8e821360c30db4
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-06-22 20:58:32 -0300

    testando endpoints, login e register ok

--- backend/backend/src/main/java/com/estudamais/backend/service/serviceimpl/AuthServiceImpl.java
+++ backend/backend/src/main/java/com/estudamais/backend/service/serviceimpl/AuthServiceImpl.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthServiceImpl implements AuthService {
     @Autowired
     private AuthenticationManager authenticationManager;

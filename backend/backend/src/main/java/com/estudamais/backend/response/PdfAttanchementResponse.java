d0ae447e8cf6cba669e9f7070f8cdcc7201d6079
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-06-26 10:42:04 -0300

    removendo rabbimq e pdf do projeto, não será necessário

--- backend/backend/src/main/java/com/estudamais/backend/response/PdfAttanchementResponse.java
+++ backend/backend/src/main/java/com/estudamais/backend/response/PdfAttanchementResponse.java
@@ -1,7 +1,5 @@
 package com.estudamais.backend.response;
 
-import com.estudamais.backend.entity.PdfAttachment;
-
 public record PdfAttanchementResponse(Long id, String fileName, String fileType) {
     public PdfAttanchementResponse(PdfAttachment attachment) {
         this(attachment.getId(), attachment.getFileName(), attachment.getFileType());

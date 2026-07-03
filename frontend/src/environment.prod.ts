48e23f5560799a9fa96c69f7f8b35ca963a816a6
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-07-03 09:48:00 -0300

    build: configurando environments nativos do angular

---
+++ frontend/src/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://estuda-mais.onrender.com' // Seu link do Render aqui
+};
--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
 
-    apiUrl: 'https://estuda-mais.onrender.com'
+    apiUrl: 'http://localhost:8080'
 };

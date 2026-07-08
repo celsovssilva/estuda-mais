6ac6f8984d329d63a47dea10d66040f6bd3d74ad
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-07-08 11:29:21 -0300

    mudando o encironments part-4

--- frontend/src/app/app/app.component.ts
+++ frontend/src/app/app/app.component.ts
@@ -6,8 +6,7 @@ import { CommonModule } from '@angular/common';
     selector: 'app-root',
     standalone: true,
     imports: [CommonModule, RouterModule], // Garanta que RouterModule está aqui
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     private router = inject(Router);
